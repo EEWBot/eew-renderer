56e46188f8631b8ebe87b0d9f6aa97e0d50a400a
yanorei32 <yanorei@hotmail.co.jp>
2024-04-13 15:39:27 +0900

    Fixup API version handling

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,1 +1,59 @@
-pub mod v1;
+use serde::Deserialize;
+
+use hmac::{Hmac, Mac};
+use sha2::Sha256;
+
+use axum::{
+    extract::{Path, Query},
+    http::{header, StatusCode},
+    response::IntoResponse,
+    Json,
+};
+
+type HmacSha256 = Hmac<Sha256>;
+
+#[derive(Debug, Deserialize)]
+pub struct DrawQuery {
+    #[serde(rename = "c")]
+    pub content: String,
+
+    #[serde(rename = "s")]
+    pub sign: String,
+}
+
+pub async fn draw(_v: crate::Version, query: Query<DrawQuery>) -> impl IntoResponse {
+    let key = crate::KEY.get().unwrap();
+
+    let mut mac = HmacSha256::new_from_slice(key).unwrap();
+    mac.update(query.content.as_bytes());
+    let s = format!("{:02x}", mac.finalize().into_bytes());
+
+    if s != query.sign {
+        println!("{s} {}", query.sign);
+        return (StatusCode::BAD_REQUEST, "Invalid Signature");
+    }
+
+    println!("{query:?}");
+
+    (StatusCode::SERVICE_UNAVAILABLE, "")
+}
+
+pub async fn maps(_v: crate::Version) -> impl IntoResponse {
+    Json(crate::map::map_names())
+}
+
+pub async fn map(_v: crate::Version, Path(map_id): Path<String>) -> impl IntoResponse {
+    let Some(map) = crate::map::query_map(&map_id) else {
+        return (
+            StatusCode::NOT_FOUND,
+            [(header::CONTENT_TYPE, "text/plain")],
+            "NOT FOUND",
+        );
+    };
+
+    (
+        StatusCode::OK,
+        [(header::CONTENT_TYPE, "application/json")],
+        map,
+    )
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 pub mod api;
 pub mod map;
 
+use std::collections::HashMap;
 use std::net::SocketAddr;
-use axum::{response::Html, routing::get, Router};
+
+use axum::{
+    async_trait,
+    extract::{FromRequestParts, Path},
+    http::{request::Parts, StatusCode},
+    response::{Html, IntoResponse, Response},
+    routing::get,
+    RequestPartsExt, Router,
+};
 use clap::Parser;
 use once_cell::sync::OnceCell;
 
@@ -21,6 +30,33 @@ async fn handler() -> Html<&'static str> {
     Html("<h1>EEW Renderer</h1>")
 }
 
+#[derive(Debug)]
+pub enum Version {
+    V1,
+}
+
+#[async_trait]
+impl<S> FromRequestParts<S> for Version
+where
+    S: Send + Sync,
+{
+    type Rejection = Response;
+
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
+        let params: Path<HashMap<String, String>> =
+            parts.extract().await.map_err(IntoResponse::into_response)?;
+
+        let version = params
+            .get("version")
+            .ok_or_else(|| (StatusCode::NOT_FOUND, "Version param missing").into_response())?;
+
+        match version.as_str() {
+            "v1" => Ok(Version::V1),
+            _ =>  Err((StatusCode::NOT_FOUND, "Unknown API version").into_response()),
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() {
     let c = Cli::parse();
@@ -28,9 +64,9 @@ async fn main() {
 
     let app = Router::new()
         .route("/", get(handler))
-        .route("/api/v1/maps", get(api::v1::maps))
-        .route("/api/v1/map/:map_id", get(api::v1::map))
-        .route("/api/v1/draw", get(api::v1::draw));
+        .route("/api/:version/maps", get(api::maps))
+        .route("/api/:version/map/:map_id", get(api::map))
+        .route("/api/:version/draw", get(api::draw));
 
     let listener = tokio::net::TcpListener::bind(c.listen).await.unwrap();
 
